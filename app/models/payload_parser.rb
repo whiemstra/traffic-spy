6ce3abb6fbc660de549bd0ba540daa87e6dca742
Whitney Hiemstra <whitcolorado@gmail.com>
2015-05-28 15:30:33 -0600

    transferred source logic to its own class sourcevalidator

---
+++ app/models/payload_parser.rb
@@ -0,0 +1,5 @@
+require 'json'
+
+class PayloadParser
+
+end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,4 @@
+
 class Source < ActiveRecord::Base
   validates :identifier, presence: true, uniqueness: true
   validates :rooturl, presence: true
