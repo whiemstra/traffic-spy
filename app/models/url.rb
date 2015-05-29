e54cc04c739b2ff70f9267b4299e64ec8dd0e652
Whitney Hiemstra <whitcolorado@gmail.com>
2015-05-29 15:41:43 -0600

    working on viewng client data

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,10 +1,13 @@
-class Url < ActiveRecord::Base
+require 'pry'
+
+class ApplicationDetails < ActiveRecord::Base
   has_many :payloads
 
   def requested_urls
     if identified_source = Source.find_by_identifier(identifier)
-      identified_source.payloads.group_by(:url)
-
+      grouped_urls = identified_source.payloads.group_by { |payload| payload[:url] }
+      sorted_urls = grouped_urls.map { |url, payloads| [url, payloads.length] }
+      descending_urls = sorted_urls.sort_by { |pair| pair[1] }
     else
       { status: 403, body: "Application Not Registered"}
     end
